8bbc018ec5c16ebc4d2600eeae78ed23630577c0
Lukasz Cepowski <lukasz@cepowski.pl>
2012-09-11 13:35:41 +0200

    curl get test

--- test/HttpTest.php
+++ test/HttpTest.php
@@ -4,21 +4,40 @@ require_once 'Mock_Http_Server.php';
 
 class HttpTest extends PHPUnit_Framework_TestCase
 {
-
-    public function testSimpleGet()
+    
+    protected $_server;
+    
+    public function setUp()
     {
         $rootPath = dirname(dirname(__FILE__));
         
-        $server = new Mock_Http_Server();
-        $server
+        $this->_server = new Mock_Http_Server();
+        $this->_server
             ->setBinDir($rootPath.'/bin')
             ->setWebDir($rootPath.'/web')
             ->start();
+    }
+    
+    public function tearDown()
+    {
+        $this->_server->stop();
+    }
+    
+    public function testGet()
+    {
+        $url = $this->_server->getBaseUrl().'/mock/get.php';
         
-        $response = file_get_contents($server->getBaseUrl().'/mock/get.php');
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $contentLength = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+        $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+        
         $this->assertEquals('ok', $response);
-                
-        $server->stop();
+        $this->assertEquals(200, $httpCode);
+        $this->assertEquals(2, $contentLength);
+        $this->assertEquals('text/html', $contentType);
     }
 
 }
